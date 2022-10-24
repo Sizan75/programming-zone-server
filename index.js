0eed712ae82a1f1868b4fb043782144829525d4b
Sizan75 <sakiuzzaman26@gmail.com>
2022-10-25 04:37:35 +0600

    course route added

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const cors= require('cors')
 const port = process.env.PORT || 5000;
 app.use(cors());
 
+const courses=require('./data/courses.json')
 
 app.get('/', (req,res)=>{
     res.send('Courses Api running')
 });
 
-
-
+app.get('/courses', (req,res)=>{
+    res.send(courses)
+});
+app.get('/courses/:id', (req,res)=>{
+    const id=req.params.id;
+        const course= news.find(c => c._id === id)
+        res.send(course) 
+    })
 app.listen(port, ()=>{
 console.log('Programming Zone running in ', port)
 })
