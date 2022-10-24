12565c5ab3889fb450704f4a1ec342cca258952e
Sizan75 <sakiuzzaman26@gmail.com>
2022-10-25 03:19:57 +0600

    initial index.js created

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const express = require('express')
 const app = express()
+
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req,res)=>{
+    res.send('Courses Api running')
+});
+
+
+
+app.listen(port, ()=>{
+console.log('Programming Zone running in ', port)
+})
