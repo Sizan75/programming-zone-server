ef44e27873d1bbcefc92259ef8efe26a1a178aa0
Sizan75 <sakiuzzaman26@gmail.com>
2022-10-26 23:15:50 +0600

    checkout api created

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const express = require('express')
 const app = express()
-const cors= require('cors')
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 app.use(cors());
 
-const courses=require('./data/courses.json')
+const courses = require('./data/courses.json')
 
-app.get('/', (req,res)=>{
-    res.send('Courses Api running')
+app.get('/', (req, res) => {
+    res.send('Courses   Api running')
 });
 
-app.get('/courses', (req,res)=>{
+app.get('/courses', (req, res) => {
     res.send(courses)
 });
-app.get('/courses/:id', (req,res)=>{
-    const id=req.params.id;
-        const course= courses.find(c => c._id === id)
-        res.send(course) 
-    })
-app.listen(port, ()=>{
-console.log('Programming Zone running in ', port)
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find(c => c._id == id)
+    res.send(course)
+})
+
+app.get('/checkout/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find(c => c._id == id)
+    res.send(course)
+})
+
+app.listen(port, () => {
+    console.log('Programming Zone running in ', port)
 })
